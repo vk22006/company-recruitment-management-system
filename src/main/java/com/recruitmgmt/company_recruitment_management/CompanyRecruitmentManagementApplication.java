7391282feee9ba32b2dc8a2ad4f418a703e4024a
vk22006 <kishorev22006@gmail.com>
2026-05-01 21:13:16 +0530

    Initial Commit

---
+++ src/main/java/com/recruitmgmt/company_recruitment_management/CompanyRecruitmentManagementApplication.java
@@ -0,0 +1,13 @@
+package com.recruitmgmt.company_recruitment_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyRecruitmentManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyRecruitmentManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recruitmgmt/company_recruitment_management/CompanyRecruitmentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recruitmgmt.company_recruitment_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyRecruitmentManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
